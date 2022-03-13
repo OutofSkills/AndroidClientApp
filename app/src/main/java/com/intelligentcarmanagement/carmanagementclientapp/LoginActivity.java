3aab216c266bf325b11362597fabe5b09988a052
OutofSkills <56539829+OutofSkills@users.noreply.github.com>
2022-03-13 19:31:23 +0200

    added shared drawer menu

--- app/src/main/java/com/intelligentcarmanagement/carmanagementclientapp/LoginActivity.java
+++ app/src/main/java/com/intelligentcarmanagement/carmanagementclientapp/LoginActivity.java
@@ -29,7 +29,7 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
+                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
             }
         });
     }
