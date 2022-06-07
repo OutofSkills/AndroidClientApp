f107d52867d286ea0f37651a66aefb4caab9971c
OutofSkills <56539829+OutofSkills@users.noreply.github.com>
2022-06-07 17:21:51 +0300

    fixed timeout problem by adding a custom httpClient for Retrofit

--- app/src/main/java/com/intelligentcarmanagement/carmanagementclientapp/api/RetrofitService.java
+++ app/src/main/java/com/intelligentcarmanagement/carmanagementclientapp/api/RetrofitService.java
@@ -2,14 +2,24 @@ package com.intelligentcarmanagement.carmanagementclientapp.api;
 
 import com.intelligentcarmanagement.carmanagementclientapp.utils.Constants;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitService {
+    private static OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
+            .connectTimeout(25, TimeUnit.SECONDS)
+            .readTimeout(25, TimeUnit.SECONDS)
+            .writeTimeout(25, TimeUnit.SECONDS)
+            .build();
+
     private static Retrofit.Builder retrofitBuilder =
             new Retrofit.Builder()
-                .baseUrl(Constants.API_URL)
-                .addConverterFactory(GsonConverterFactory.create());
+                    .baseUrl(Constants.API_URL)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create());
 
     private static Retrofit retrofit = retrofitBuilder.build();
 
